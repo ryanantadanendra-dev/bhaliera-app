d5aee7e31491321275437b8dabeecaf9467bc18c
Ryananta Danendra <ryanantadanendra887@gmail.com>
2026-03-12 20:26:20 +0800

    Fixing home page title

--- breeze-next/src/app/(site)/page.js
+++ breeze-next/src/app/(site)/page.js
@@ -89,8 +89,9 @@ const Home = () => {
             <header className="relative min-h-screen md:min-h-full lg:h-screen text-center md:py-32 lg:py-0 md:text-left flex flex-col justify-center px-8 lg:px-0 sm:items-center md:items-start pt-0 bg-[#082841] overflow-hidden">
                 {/* Main Content - This should be LCP element */}
                 <h1 className="text-white text-3xl md:text-4xl lg:text-5xl lg:text-left ms-0 lg:ms-10 lg:pe-[18rem] md:pe-32 z-30">
-                    <span>Your Trusted Partner in Legal,</span>
+                    <span>Your Trusted Partner in Legal, </span>
                     <span className="mt-0 md:mt-4 md:block">
+                        {' '}
                         Licensing, and Certification
                     </span>
                     <span className="mt-0 md:mt-4 md:block"> Solutions.</span>
